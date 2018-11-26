c20f2f54d9e8c11a117f29e19d9bbc74bdcb28a7
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-11-26 16:00:01 +0100

    generate uploads controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'uploads/index'
+  get 'uploads/new'
+  get 'uploads/create'
+  get 'uploads/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
