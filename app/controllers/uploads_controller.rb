b3cf5d6720a93c6af460040014f66e296f006c77
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-11-26 21:52:07 +0100

    handle file upload and display uploaded files

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -4,9 +4,20 @@ class UploadsController < ApplicationController
 
   def new
     @upload = Upload.new
+    @uploads = [].tap do |files|
+      Upload.all.each { |upload| upload.files.each { |file| files << { file: file } } }
+    end
   end
 
   def create
+    upload = Upload.new(upload_params)
+    if upload.save
+      redirect_to root_path
+      flash[:success] = "File(s) successfully uploaded."
+    else
+      redirect_to root_path
+      flash[:danger] = "An error occurred. Please try again."
+    end
   end
 
   def destroy
