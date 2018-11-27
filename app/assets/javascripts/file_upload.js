e4d4ba7d38170e613ae3f57634d209d41a8ac53e
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-11-27 12:43:02 +0100

    feat: display file upload progress bar

--- app/assets/javascripts/file_upload.js
+++ app/assets/javascripts/file_upload.js
@@ -4,11 +4,37 @@ document.addEventListener("turbolinks:load", event => {
   form.onchange = formEvent => {
     Object.values(formEvent.target.files)
       .map(file => file.name)
-      .forEach(fileName => {
-        let fileEntry = document.createElement('p');
-        fileEntry.className = 'selected-file';
-        fileEntry.innerHTML = fileName;
+      .forEach((fileName, index) => {
+        let fileEntry = document.createElement("div");
+        fileEntry.className = "progress";
+        fileEntry.id = `direct-upload-${index + 1}`;
+        let progress = document.createElement("div");
+        progress.className = "progress-bar bg-success";
+        progress.id = `direct-upload-progress-${index + 1}`;
+        progress.style.width = "0%";
+        let fileEntrySpan = document.createElement("span");
+        fileEntrySpan.innerHTML = fileName;
+        fileEntrySpan.style.color = "black";
+        progress.appendChild(fileEntrySpan);
+        fileEntry.appendChild(progress);
         selectedFiles.appendChild(fileEntry);
       });
   };
+});
+
+addEventListener("direct-upload:progress", event => {
+  const { id, progress } = event.detail;
+  const progressElement = document.getElementById(
+    `direct-upload-progress-${id}`
+  );
+  progressElement.style.width = `${progress}%`;
+  progressElement.firstElementChild.style.color = "white";
+});
+
+addEventListener("direct-upload:error", event => {
+  event.preventDefault();
+  const { id, error } = event.detail;
+  const element = document.getElementById(`direct-upload-${id}`);
+  element.classList.add("direct-upload--error");
+  element.setAttribute("title", error);
 });
